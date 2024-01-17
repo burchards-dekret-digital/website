0ff1af8bab9c4b806ab04a5726499a6b84ba59c9
michela parma <44635720+michelaparma@users.noreply.github.com>
2024-01-17 17:35:43 +0100

    creation of mss and ms-item pages

---
+++ js/mirador-singleManuscript.js
@@ -0,0 +1,65 @@
+var mirador = Mirador.viewer({
+            id: "mirador-singleMS",
+            language: 'de',
+            window: { //global window defaults
+                allowClose: true, // Configure if windows can be closed or not
+                defaultView: 'single',  // Configure which viewing mode (e.g. single, book, gallery) for windows to be opened in
+                highlightAllAnnotations: true, // Configure whether to display annotations on the canvas by default
+                panels: { // Configure which panels are visible in WindowSideBarButtons
+                    info: true,
+                    attribution: true,
+                    canvas: true,
+                    annotations: true,
+                    search: true,
+                    layers: true
+                },
+                views: [
+                    { key: 'single', behaviors: ['individuals'] },
+                    { key: 'book', behaviors: ['paged'] }
+                    
+               ] },
+            windows: [{
+                /* loadedManifest: <div data-template="app:iiif-manifest"/>,
+                canvasId: <div data-template="app:iiif-canvas-id-dekret"/> 
+                 *  highlightAllAnnotations: true // Configure whether to display annotations on the canvas by default*/
+              }],
+              
+              thumbnails: {
+                    preferredFormats: ['jpg', 'png', 'webp', 'tif']
+                    },
+            thumbnailNavigation: {
+                defaultPosition: 'far-bottom', // Which position for the thumbnail navigation to be be displayed. Other possible values are "far-bottom" or "far-right"
+                displaySettings: true // Display the settings for this in WindowTopMenu
+                },
+            catalog: [
+                { manifestId: 'https://burchard.adwmainz.net/exist/rest/db/collections/bdd/data/manifests/frankfurt-ub-b-50-manifest.json' },
+                { manifestId: 'https://burchard.adwmainz.net/exist/rest/db/collections/bdd/data/manifests/vatican-bav-pl-585-manifest.json' },
+                { manifestId: 'https://burchard.adwmainz.net/exist/rest/db/collections/bdd/data/manifests/vatican-bav-pl-586-manifest.json'},
+                { manifestId: 'https://burchard.adwmainz.net/exist/rest/db/collections/bdd/data/manifests/bamberg-sb-c-6.json'},
+                { manifestId: 'https://burchard.adwmainz.net/exist/rest/db/collections/bdd/data/manifests/koeln-edd-119-manifest.json'}
+            ]
+        });
+
+    
+ 
+/*    <!-- changing page in Mirador-viewer after click on folia --> */
+    
+        function changecanvas(canvasid, manifest) {
+        var mirador = Mirador.viewer({
+            id: "mirador-singleMS",
+            windows: [{
+                loadedManifest: manifest,
+                canvasId: canvasid,
+                thumbnailNavigationPosition: "far-bottom"
+              }],
+            catalog: [
+                { manifestId: 'https://burchard.adwmainz.net/exist/rest/db/collections/bdd/data/manifests/frankfurt-ub-b-50-manifest.json' },
+                { manifestId: 'https://burchard.adwmainz.net/exist/rest/db/collections/bdd/data/manifests/vatican-bav-pl-585-manifest.json' },
+                { manifestId: 'https://burchard.adwmainz.net/exist/rest/db/collections/bdd/data/manifests/vatican-bav-pl-586-manifest.json'},
+                { manifestId: 'https://burchard.adwmainz.net/exist/rest/db/collections/bdd/data/manifests/bamberg-sb-c-6.json'},
+                { manifestId: 'https://burchard.adwmainz.net/exist/rest/db/collections/bdd/data/manifests/koeln-edd-119-manifest.json'}
+            ]
+          });}
+          
+
+    
